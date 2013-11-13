bbf2e74d286c3a48af45038b05102d617c6addcc
Clark Ritchie <clarkritchie@yahoo.com>
2013-11-13 15:27:31 -0800

    1st attempt to do things in parallel

--- nexus7parallel.py
+++ nexus7parallel.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python
+#!/Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
+
+#!/usr/bin/env python
 #
 # A simple script to:
 #  - Install a directory worth of APKs onto N-attached Android devices via USB
 #  - Flash N-attached Android devices via USB with unlocked bootloader
 #
-# Usage:  primr.py -- Will install all APKs in the directory path hard coded below
-#         primr.py flash -- Will flash using the bootloader and Android OS image hard coded below
+# Usage:  nexus7parallel.py -- Will install all APKs in the directory path hard coded below
+#         nexus7parallel.py flash -- Will flash using the bootloader and Android OS image hard coded below
 #
 #
 # This program is free software: you can redistribute it and/or modify
@@ -22,25 +24,25 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import subprocess, sys, re, time
-from os import listdir
-from os.path import isfile, join
-# http://stackoverflow.com/questions/7207309/python-how-can-i-run-python-functions-in-parallel
-from multiprocessing import Process
+import subprocess, sys, re, time, glob
+# @see http://stackoverflow.com/questions/7207309/python-how-can-i-run-python-functions-in-parallel
+# from multiprocessing import Process
+from multiprocessing import Pool
 
-# Paths to update:
+# BEGIN Paths to update:
 # path to adb executable
-adb='/Users/clarkritchie/android-sdk/sdk/platform-tools/adb'
+adb='/usr/local/bin/android-sdk/sdk/platform-tools/adb'
 # path to fastboot executable
-fastboot='/Users/clarkritchie/android-sdk/sdk/platform-tools/fastboot'
+fastboot='/usr/local/bin/android-sdk/sdk/platform-tools/fastboot'
 # path to directory of APK files to install
-apk_files='/Users/clarkritchie/Dropbox/Work/Inveneo/PRIMR/APKs'
-# path to the Android OS image file
-android_image='/Users/clarkritchie/Dropbox/Work/Inveneo/PRIMR/firmware/nakasi-jwr66y-diffbootload/image-nakasi-jwr66y.zip'
-bootloader='/Users/clarkritchie/Dropbox/Work/Inveneo/PRIMR/firmware/nakasi-jwr66y-diffbootload/bootloader-grouper-4.23.img'
+apk_files='/tmp/nexus7/APKs'
+# path to the Android OS image file and bootloader
+android_image='/tmp/nexus7/image-nakasi-jwr66y.zip'
+bootloader='/tmp/nexus7/bootloader-grouper-4.23.img'
+# END Paths to update:
 
 def flashTablet( device_id, fastboot, bootloader, android_image ):
-    print 'Beginning flash tablet of %s' % device_id
+    print("Begin flash of tablet %s", device_id)
     # for i in xrange(10000000): pass
     flash_cmds = [
         '%s -s %s oem unlock' % ( fastboot, device_id ),
@@ -55,30 +57,38 @@ def flashTablet( device_id, fastboot, bootloader, android_image ):
         '%s -s %s -w update %s' % ( fastboot, device_id, android_image ),
     ]
     for cmd in flash_cmds:
-        print cmd
-        time.sleep(0.1)
-    print 'End flash tablet of %s' % device_id
+        print( cmd)
+        p = subprocess.Popen( cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+            print (line)
+        retval = p.wait()
+        # time.sleep(5)
+        print ('End flash of tablet %s', device_id)
 
 def installAPKs( device_id, apks ):
-    print 'Beginning APK install on tablet of %s' % device_id
-    # for i in xrange(10000000): pass
-    
-    for apk in apks:
-        print apk
-        time.sleep(0.1)
-        print 'Installing %s onto %s' % ( apk, device_id )
-        # ignore Mac .DS_Store and any other hidden files
-        if not f.startswith('.'):
-            # maybe have to change this from / to \ for Windows?
-            cmd=adb + ' -s %s install -r %s/%s' % (device_id,apk_files,apk)
-            p = subprocess.Popen( cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            for line in p.stdout.readlines():
-                print line
-            retval = p.wait()
-    print 'End APK install on tablet of %s' % device_id
+    print ( 'Begin APK install on tablet %s' % device_id )
+    for f in apks:
+        print(f)
+        
+    for f in apks:
+        print( 'Installing %s onto %s' % ( f, device_id ))
+        cmd=adb + ' -s '+device_id+" install -r "+f;
+        print(cmd)
+        subp = subprocess.Popen( [adb,"-s",device_id,"install","-r",f], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+        stdout, stderr = subp.communicate() 
+        subp.wait()
+        print (stdout)
+        # time.sleep(10)
+        print( re.sub("[\t\s\s+]" , " ", stdout ))
+        print( re.sub("[\t\s\s+]" , " ", stderr ))
+        print( subp.returncode )
+    print( "End APK install on tablet %s" % device_id )
+    return 0
 
     
 if __name__ == '__main__':
+    
+    start_time = time.time()
     
     # if the argument is "flash" then flash the device
     flash=False
@@ -90,14 +100,15 @@ if __name__ == '__main__':
             flash=True
     
     if flash:
-        print "Flash attached tablets"
+        print ("Flash attached tablets")
         cmd=fastboot + " devices"
     else:
-        print "Install APKs on attached tablets"
+        print ("Install APKs on attached tablets")
         cmd=adb + " devices"
     
     # get all the APKs into a list
-    apks = [ f for f in listdir(apk_files) if isfile(join(apk_files,f)) ]
+    # apks = [ f for f in listdir(apk_files) if isfile(join(apk_files,f)) ]
+    apks = glob.glob( apk_files+"/*.apk" ) # revisit for case sensitivity
     
     # get a list of device IDs for all attached tablets
     devices = []
@@ -105,25 +116,28 @@ if __name__ == '__main__':
     for line in p.stdout.readlines():
         # print line
         # device_id = re.match( '^([%&+ \w]+).*device$', line )
-        device_id = re.match( '^([%&+ \w]+).*[a-z]+$', line )
+        device_id = re.match( '^([%&+ \w]+).*[a-z]+$', line.decode("latin1") )
         # for each device, install the APKs one by one
         if device_id is not None:
             devices.append(device_id.group(1))
     retval = p.wait()
     
-    devices = [ 10,4 ]
-    
+    # setup the pool
+    pool = Pool(processes=len(devices)) # argument is number of processes, default is the number of CPUs
+
     for device_id in devices:
         if device_id is not None:
-            print "Using Device ", device_id
-            
+            print ( "Using tablet %s" % device_id)
             if flash:
-                print "y"
-                #p1 = Process(target=flashTablet, args=( device_id, fastboot, bootloader, android_image ))
-                #p1.start()
-                # p1.join()
+                print( "flash")
+                #p = mp.Process( target=flashTablet, args=( device_id, fastboot, bootloader, android_image ))
+                #processes.append( p )
+                #p.start()
             else:
-                print "x"
-                p1 = Process(target=installAPKs, args=( device_id, apks, apk_files ))
-                p1.start()
-                
+                pool.apply_async( installAPKs, args = ( device_id, apks ))
+                #pool.map_async(installAPKs, [device_id, apks, apk_files])
+    pool.close()
+    pool.join()
+
+    elapsed_time = time.time() - start_time
+    print( '%s completed - %s tablet(s) in %.03f seconds\n' % ( sys.argv[0], len(devices), elapsed_time ))
